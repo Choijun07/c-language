df4701c99e40d2bfbbb52aa92203a8c3165f5468
그거 몰라?그거 알아? (알아몰라) <shhxjcje@gmail.com>
2023-04-06 05:27:34 +0000

    main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello, goorm!\n");
+	printf(" 컴퓨터과 1학년3반\n");
+	printf(" 10321최준\n");
+	printf(" 10323황수연\n");
+	printf(" 10310박찬진\n");
+	
+	return 0;
+}
