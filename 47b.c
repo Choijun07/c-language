e639163531c5e5f84c168a781fe6c886b7bbae5a
그거 몰라?그거 알아? (알아몰라) <shhxjcje@gmail.com>
2023-04-06 05:25:35 +0000

    47b.c

---
+++ 47b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num1=16;
+	int unm2;
+	int snm=0; int div; int mul;
+	unm2 = 61;
+	snm = num1 + unm2;
+	printf("%d + %d = %d" ,unm2 ,unm2 ,snm);
+	
+	return 0;
+}
